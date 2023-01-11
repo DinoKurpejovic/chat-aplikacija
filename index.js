117369287bca9700ee5a838f6091bad110f02903
Dino Kurpejovic <dinookurpejovic@gmail.com>
2023-01-11 17:27:59 +0100

    chat aplikacija56222

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 //Funkcija express okvira za analizu http zahtjeva
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
