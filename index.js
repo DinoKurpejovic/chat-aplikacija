1429af65bd185b46a11679dbca774becfd28e893
Dino Kurpejovic <dinookurpejovic@gmail.com>
2023-01-11 18:14:16 +0100

    chat aplikacija5632

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoutes = require("./routes/auth.js");
 
 const app = express();
 //Ručno postavljanje porta
+const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
@@ -21,3 +22,4 @@ app.get('/', (req, res) => {
 
 app.use('/auth', authRoutes);
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
